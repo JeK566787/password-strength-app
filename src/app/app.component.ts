a2f98bd66e409c545d6cf2108c15a0053026878b
Evgeniy Pavlichenko <92581680+JeK566787@users.noreply.github.com>
2024-08-15 12:56:59 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PasswordStrengthComponent } from './password-strength/password-strength.component'
+import { NgModel } from '@angular/forms'; // Ensure to import NgModel if necessary
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PasswordStrengthComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
